fb04bf846bd81e6ce787c6469d09608845db26d8
Donovan Tong <donotong01@gmail.com>
2022-09-29 16:17:04 -0600

    I did not double check my code. Here is the final result

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -204,7 +204,7 @@ switch (letterGrade) {
 //Code Here
 
 for (i=1; i<=100; i++) {
-  if (i%15 === 0) {
+  if (i%3 ===0 && i%5===0) {
     console.log("Devmountain")
   } else if (i%5 === 0) {
     console.log("Mountain")
